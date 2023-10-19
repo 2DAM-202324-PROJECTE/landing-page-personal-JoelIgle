5f82fccb4405fe9c94ec390c8d5271f6e8867467
JoelIgle <joeliglesias@iesebre.com>
2023-10-19 18:50:26 +0200

    Prova de multi page

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
 export default {
 // config options
-    base: '/landing-page-personal-JoelIgle/'
+    base: '/landing-page-personal-JoelIgle/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                company: resolve(__dirname, 'Company/company.html'),
+            },
+        },
+    },
 }
