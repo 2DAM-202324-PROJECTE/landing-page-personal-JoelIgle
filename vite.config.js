e1b76ebf6c055b9a0d0a13441b394c18da14796f
JoelIgle <joeliglesias@iesebre.com>
2023-10-19 18:53:18 +0200

    prova

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
 export default {
 // config options
-    base: '/landing-page-personal-JoelIgle/',
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                company: resolve(__dirname, 'Company/company.html'),
-            },
-        },
-    },
+    base: '/landing-page-personal-JoelIgle/'
+
 }
